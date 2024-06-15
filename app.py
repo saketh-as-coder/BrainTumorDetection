64cde33bacadea9c81c15e0c80029e5ff29ca51b
Saketh Ram <129545822+saketh-as-coder@users.noreply.github.com>
2024-06-16 00:33:21 +0530

    updated_app.py
    add some extra ui with streamlit

--- app.py
+++ app.py
@@ -2,15 +2,13 @@ import streamlit as st
 from PIL import Image
 import tensorflow as tf
 import numpy as np
-
-# Load your trained model
 model = tf.keras.models.load_model('brain_tumor.h5')
 
 # Function to preprocess the image
 def preprocess_image(image):
-    image = image.resize((150,150))  # Adjust size as per your model requirement
+    image = image.resize((150,150))  
     img_array = np.array(image)
-    img_array=img_array.reshape(1,150,150,3)  # Normalize if required
+    img_array=img_array.reshape(1,150,150,3)  
     return img_array
 
 # Function to predict the image
@@ -19,24 +17,34 @@ def predict(image):
     x = model.predict(processed_image)
     ind=x.argmax()
     if(ind==0):
-        return ("glioma_tumor")
+        return ("This might be glioma tumor")
     elif(ind==1):
-        return("meningioma_tumor")
+        return("This might be meningioma tumor")
     elif(ind==2):
-        return("no_tumor")
+        return("There isn't any tumor")
     elif(ind==3):
-        return("pituitary_tumor")
+        return("This might be pituitary tumor")
     
 
 
-st.title("Image Classification App")
+st.title('Brain tumor detector')
+pages = ['Home', 'Predictor']
+selected_page = st.sidebar.radio('Select a page', pages)
 
-uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+if selected_page == 'Home':
+    st.header('Welcome to the Brain tumor detector!')
+    st.write('This app allows you to detect the type of tumor from report image.')
+elif selected_page == 'Predictor':
+    st.header('Image Predictor')
+    uploaded_file = st.file_uploader('Upload an image', type=['jpg', 'jpeg', 'png'])
 
-if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    st.image(image, caption='Uploaded Image.', use_column_width=True)
-    st.write("")
-    st.text("Predicting...")
-    prediction = predict(image)
-    st.write(f'{prediction}')
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        st.image(image, caption='Uploaded Image', use_column_width=True)
+        st.write('')
+
+        if st.button('Predict'):
+            label = predict(image)
+            st.write(f'{label}')
+
+
